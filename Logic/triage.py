855a7bb7329339b6b0bad5f0e3e7190291cc8e61
TaraAlexander <alxtar003@myuct.ac.za>
2026-05-15 22:28:39 +0200

    Updated questionnaire ui

--- Logic/triage.py
+++ Logic/triage.py
@@ -1,6 +1,4 @@
 #Triage messages based on each menolikelihood scores
-
-<<<<<<<<< Temporary merge branch 1
 # Output templates for MenoScreen
 # This file stores all triage messages and output content
 # after questionnaire scoring is completed
@@ -206,31 +204,4 @@ def format_clinician_summary_message(percentage_score, band):
         )
 
     return "Error: Band not found in templates."
-=========
-def determine_triage(percentagescore):
 
-#message for a menolikelihood of 0-30%
-    if 0<= percentagescore <=30:
-     message = "Her symptom profile does not strongly suggest hormonal change at this time. Reassure her and invite her to return if symptoms persist or worsen"
-     clinician_summary = "\n\nSummary\n\n Low MenoLikelihood score. \nNo significant symptom cluster identified at this time. \nRecommend: reassurance and routine follow-up."
-    
-#message for menolikelihood of 31-55%
-    elif 31<= percentagescore<=55:
-       message = "Her symptom profile is consistent with early perimenopause. Begin the menopause conversation. Consider lifestyle support or referral."
-       clinician_summary = "\n\nSummary\n\nModerate MenoLikelihood score.\n Symptom cluster consistent with perimenopause. \nRecommend: menopause conversation, lifestyle support, consider referral."
-
-    
-#message for menolikelihood of 56-80
-    elif 56<=percentagescore<=80:
-       message = "Her symptom profile is strongly consistent with peri/menopause. Discuss HRT options or refer to a menopause-trained GP or gynaecologist"
-       clinician_summary = "\n\nSummary\n\nHigh MenoLikelihood score. \nStrong symptom pattern consistent with perimenopause or menopause. \nRecommend: discussion of HRT options or referral to menopause-trained specialist."
-   
-#message for menolikelihood of 81-100
-    else:
-       message = "She is carrying a significant symptom burden. Urgent referral is recommended. Please use warm, validating language in your conversation and in her summary"
-       clinician_summary = "\n\nSummary\n\nVery high MenoLikelihood score.\nSevere symptom burden. \nRecommend: Urgent referral to menopause-trained specialist. Patient requires compassionate, timely support."
-       
-    
-    return message + clinician_summary 
-
->>>>>>>>> Temporary merge branch 2
--- User-Interface/questionnaire_window.py
+++ User-Interface/questionnaire_window.py
@@ -3,4 +3,3 @@ import sys
 from PyQt6 import uic
 
 uic.loadUi("User-Interface/questionnaire_window.ui", self)
-
